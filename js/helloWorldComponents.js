d8f5f168785830b5f09713b54996f947b06c1413
Shing Lyu <shing.lyu@gmail.com>
2018-02-08 22:53:39 +0100

    Updated to new react library

--- js/helloWorldComponents.js
+++ js/helloWorldComponents.js
@@ -1,7 +1,9 @@
 /* Deliberatly exposing all of them in global name space */
+// Create an alias for React.createElement to save some typing
 var h = React.createElement;
-var MyList =  React.createClass({
-  render: function(){
+
+class MyList extends React.Component {
+  render() {
     var lis = this.props.data.map(function(text, idx){
       return h('li', {key:idx }, text)
     })
@@ -9,15 +11,15 @@ var MyList =  React.createClass({
       h('ul', null, lis)
     )
   }
-})
+}
 
-var HelloWorldBanner =  React.createClass({
-  render: function(){
+class HelloWorldBanner extends React.Component {
+  render(){
     return (
       h('div', null, 
         h('h1', null, 'Hello World'),
-        h(MyList, {data:['hi','I am','Mozilla']})
+        h(MyList, { data: ['Hi', 'I am', 'Shing Lyu'] })
        )
     )
   }
-})
+}
--- js/main.js
+++ js/main.js
@@ -1,2 +1,5 @@
 /* A one-liner, you can use inline js in html if you like */
-ReactDOM.render(React.createElement(HelloWorldBanner), document.getElementById('content'))
+ReactDOM.render(
+  React.createElement(HelloWorldBanner), 
+  document.getElementById('content')
+)
